ff42043cd3ef19e57f489d3b32c937cf7713c540
Roni Axelrad <roni.axelrad@sapiens.com>
2018-01-03 15:20:30 -0600

    Add project files.

---
+++ ReporterConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReporterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
